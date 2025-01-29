c393d67d19c052725da9e6656c2d1522d33a89bb
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2025-01-28 21:44:19 -0300

    [IMP] project/clvsol_odoo15_lib/clv_patient_marker.py

--- project/clvsol_odoo15_lib/clv_patient_marker.py
+++ project/clvsol_odoo15_lib/clv_patient_marker.py
@@ -32,11 +32,6 @@ def get_sqlite(server_url, db_name, username, password, initialize=False):
         )
         clv_patient_marker = pd.DataFrame(clv_patient_marker_objects)
 
-        # for i, row in clv_patient_marker.iterrows():
-
-        #     if row['description'] is False:
-        #         clv_patient_marker['description'].values[i] = None
-
         conn = sqlite3.connect('data/jcafb_2025.db')
 
         if initialize:
@@ -51,14 +46,14 @@ def get_sqlite(server_url, db_name, username, password, initialize=False):
 
             clv_patient_marker.to_sql('clv_patient_marker', conn, if_exists='append', index=False)
 
-            sql = '''
-                UPDATE clv_patient_marker
-                SET description = NULL
-                WHERE description = '0';
-                '''
-            cur = conn.cursor()
-            cur.execute(sql)
-            conn.commit()
+        sql = '''
+            UPDATE clv_patient_marker
+            SET description = NULL
+            WHERE description = '0';
+            '''
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
 
         conn.close()
 
